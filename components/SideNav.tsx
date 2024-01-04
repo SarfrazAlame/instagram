568181a86c0031b4bf956bbae64819a6575742cc
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-01-04 11:32:38 +0530

    ui

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -6,11 +6,11 @@ import NavLink from "./NavLink";
 const SideNav = () => {
   const pathname = usePathname();
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex flex-col px-3 py-4 md:px-2">
       <div
-        className="border-t -mt-3 md:ml-0 bg-white dark:bg-neutral-950 h-16 justify-evenly fixed z-50 flex-1 -ml-3 w-full md:relative
+        className="border-t -mt-3 md:ml-0 bg-white dark:bg-neutral-950 h-16  fixed z-50 flex-1 -ml-3 w-full md:relative
       md:h-full bottom-0 
-      md:border-none flex flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2"
+      md:border-none flex flex-row gap-5  md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2"
       >
         <Logo />
         <NavLink />
