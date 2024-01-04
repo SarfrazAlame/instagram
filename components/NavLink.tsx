3da4da71ef6ce62c70c85274efb8843cb807db65
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-01-04 11:01:38 +0530

    sidebar

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+"use client";
+import {
+  Clapperboard,
+  Compass,
+  Home,
+  MessageCircle,
+  PlusSquare,
+  Search,
+} from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "Home",
+    href: "/dashboard",
+    icon: Home,
+  },
+  {
+    name: "Search",
+    href: "/dashboard/search",
+    icon: Search,
+    hideOnMobile: true,
+  },
+  {
+    name: "Explore",
+    href: "/dashboard/explore",
+    icon: Compass,
+  },
+  {
+    name: "Reels",
+    href: "/dashboard/reels",
+    icon: Clapperboard,
+  },
+  {
+    name: "Message",
+    href: "/dashboard/message",
+    icon: MessageCircle,
+  },
+  {
+    name: "Notifications",
+    href: "/dashboard/notificaions",
+    icon: Compass,
+  },
+  {
+    name: "Create",
+    href: "/dashboard/create",
+    icon: PlusSquare,
+  },
+];
 
 const NavLink = () => {
+  const pathname = usePathname();
   return (
-    <div>NavLink</div>
-  )
-}
+    <div>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        const isActive = pathname === link.href;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={buttonVariants({
+              variant: isActive ? "secondary" : "ghost",
+              className: cn("navLink", { "hidden md:flex": link.hideOnMobile }),
+              size: "lg",
+            })}
+          >
+            <LinkIcon className="w-6" />
+            <p
+              className={`${cn("hidden lg:block", {
+                "font-extrabold": isActive,
+              })}`}
+            >
+              {link.name}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
-export default NavLink
+export default NavLink;
--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,26 +1,19 @@
 "use client";
-import { Home, Link } from "lucide-react";
 import { usePathname } from "next/navigation";
 import Logo from "./Logo";
 import NavLink from "./NavLink";
-
-const links = [
-  {
-    name: "Home",
-    href: "/dasboard",
-    icon: Home,
-  },
-];
-
+ 
 const SideNav = () => {
   const pathname = usePathname();
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <div className="border-t -mt-3 md:ml-0 bg-white dark:bg-neutral-950 h-16 justify-evenly fixed z-50 flex-1 -ml-3 w-full md:relative
+      <div
+        className="border-t -mt-3 md:ml-0 bg-white dark:bg-neutral-950 h-16 justify-evenly fixed z-50 flex-1 -ml-3 w-full md:relative
       md:h-full bottom-0 
-      md:border-none flex flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2">
+      md:border-none flex flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2"
+      >
         <Logo />
-        <NavLink/>
+        <NavLink />
         {/* * user && profilelinks */}
       </div>{" "}
     </div>
