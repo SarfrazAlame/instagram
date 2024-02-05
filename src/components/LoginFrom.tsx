c1cd7316dccdfdabe32467502ec0097a7bf00623
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-02-05 09:45:04 +0530

    dark light

--- src/components/LoginFrom.tsx
+++ src/components/LoginFrom.tsx
@@ -7,8 +7,8 @@ import { Button } from "./ui/button";
 export default function LoginForm() {
   return (
     <div className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-500 px-6 pb-4 pt-8">
-        <h1 className={`mb-3 text-2xl dark:text-black`}>
+      <div className="flex-1 rounded-lg bg-gray-800 dark:bg-gray-50 px-6 pb-4 pt-8">
+        <h1 className={`mb-3 text-2xl text-white dark:text-black`}>
           Please log in to continue.
         </h1>
 
